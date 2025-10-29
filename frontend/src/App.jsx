6c868712d2779c402586d4f77d702a68548a074c
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-10-29 17:39:50 +0800

    mobile and desktop screensize change layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import { Container, Button, Alert } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
 
 function App() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
+
+    useEffect(() => {
+        const handleScreenSize = () => setIsMobile(window.innerWidth < 768);
+        window.addEventListener("resize", handleScreenSize);
+        return () => window.removeEventListener("resize", handleScreenSize);
+    }, [])
 
     return (
-        <Container className="containter-lg bg-primary w-100">
-            <span>
-                Landing Page...
-            </span>
-        </Container>
+        <>
+            {isMobile ? 
+                <Container fluid>
+                    Mobile 
+                </Container>
+                :
+                <Container fluid>
+                    Desktop
+                </Container>
+            }
+        </>
     )
 }
 
