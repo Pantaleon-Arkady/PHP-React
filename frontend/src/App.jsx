1bbb8126bbde08d591d0320bf24b0b56f229083f
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-10-29 20:34:45 +0800

    landing page/register and log in init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
     const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
@@ -17,8 +18,21 @@ function App() {
                     Mobile 
                 </Container>
                 :
-                <Container fluid>
-                    Desktop
+                <Container fluid className="row p-5 min-vh-100">
+                    <div className="col-md-7 d-flex flex-column border">
+                        <div className="h-50 border">
+
+                        </div>
+                        <div className="h-50 border">
+
+                        </div>
+                    </div>
+                    <div className="col-md-5 border">
+                        <form></form>
+                        <div>
+                            Please 
+                        </div>
+                    </div>
                 </Container>
             }
         </>
