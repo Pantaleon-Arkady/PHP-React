7f251c60bbf6fe6aaa8c55567f53d7a07d1a5938
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-05 13:40:13 +0800

    checked if database class actually works...

--- backend/projectSrc/src/Controllers/General.php
+++ backend/projectSrc/src/Controllers/General.php
@@ -14,10 +14,21 @@ class General
         $this->connection = Database::getConnection();
     }
 
+    public static function fastPrint($array)
+    {
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
+
     public function usersDisplay()
     {
 
-        
+        $q = Database::fetchAll(
+            'SELECT * FROM app_user' 
+        );
+
+        $this->fastPrint($q);
 
         include_once __DIR__ . '/../trials.php';
     }
