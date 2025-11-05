e6ada02c7be630fbbfa7279198d94ef4eb5e87d0
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-05 16:01:20 +0800

    register form, create account function

--- frontend/src/forms/register.jsx
+++ frontend/src/forms/register.jsx
@@ -1,25 +1,54 @@
 import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { createAccount } from "../service/UserService";
 
-function RegisterForm() {
+function RegisterForm({ onCreate }) {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleSubmit = () => {
-        
+    async function handleSubmit(e) {
+        e.preventDefault();
+        if (!username.trim() && !email.trim() && !password.trim()) return;
+
+        try {
+
+            const result = await createAccount({ 
+                username, email, password
+            });
+            console.log("POST /user-register result:", result);
+
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
         <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
-            {/* Example Input Field */}
             <Form.Control
-                type="text"                // e.g. "text", "number", "date"
-                placeholder="Enter something here..."                  // optional, focuses this input first
+                type="email"
+                placeholder="Enter your email here..."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
         </Form.Group>
 
         <Form.Group className="mb-3">
             <Form.Control
                 type="text"
-                placeholder="Another field..."
+                placeholder="Enter a username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+            <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
         </Form.Group>
 
