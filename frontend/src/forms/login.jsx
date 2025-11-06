708e7b283fb0bcae33f12e0104e86dfa3ed61f34
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-06 13:51:06 +0800

    log in form component

---
+++ frontend/src/forms/login.jsx
@@ -0,0 +1,54 @@
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { logInAccount } from "../service/UserService";
+
+function LogInForm({ onCreate }) {
+    const [namemail, setNameMail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        if (!namemail.trim() && !password.trim()) return;
+
+        try {
+
+            const result = await logInAccount({
+                namemail, password
+            });
+            console.log("POST /user-log-in result:", result);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <Form onSubmit={handleSubmit} className="w-100">
+            <Form.Group className="mb-3">
+                <Form.Control
+                    type="text"
+                    placeholder="Enter your username or email here..."
+                    value={namemail}
+                    onChange={(e) => setNameMail(e.target.value)}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </Form.Group>
+
+            <div className="d-flex justify-content-center mt-3">
+                <Button variant="primary" type="submit">
+                    Log In
+                </Button>
+            </div>
+        </Form>
+    )
+}
+
+export default LogInForm;
