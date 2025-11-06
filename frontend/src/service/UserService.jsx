05402980a5df2d28853315b1548d08cf2850b2e5
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-06 13:52:21 +0800

    log in function reques

--- frontend/src/service/UserService.jsx
+++ frontend/src/service/UserService.jsx
@@ -1,4 +1,5 @@
 const url = 'http://localhost:8080/pr-users';
+const logUrl = 'http://localhost:8080/pr-users/login';
 
 export async function createAccount(payload) {
     try {
@@ -10,6 +11,26 @@ export async function createAccount(payload) {
 
         if (!res.ok) {
             return { success: false, error: json.error || json.message || "Create failed" };
+        }
+        
+        const data = await res.json();
+
+        return { success: true, data: data };
+    } catch (err) {
+        return { success: false, error: err.message };
+    }
+}
+
+export async function logInAccount(payload) {
+    try {
+        const res = await fetch(logUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload)
+        })
+
+        if (!res.ok) {
+            return { success: false, error: json.error || json.message || "Log in failed" };
         }
         
         const data = await res.json();
