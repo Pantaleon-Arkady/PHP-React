2a4a5af553b31269c7ea672505b6d18ec88e087d
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-10 14:43:07 +0800

    homepage head layout

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,8 +2,18 @@ function Homepage() {
 
     return (
         <div className="">
-            <div className="homepage_head w-100 border">
-                Nav Bar
+            <div className="homepage_head w-100 border d-flex flex-row">
+                <div className="homepage_head_one border d-flex justify-content-center align-items-center">
+                    <h2>App Name</h2>
+                </div>
+                <div className="homepage_head_two border d-flex justify-content-center align-items-center">
+                    <div className="homepage_head_center border"></div>
+                </div>
+                <div className="homepage_head_three border d-flex justify-content-around align-items-center">
+                    <div className="homepage_head_features border"></div>
+                    <div className="homepage_head_features border"></div>
+                    <div className="homepage_head_features border"></div>
+                </div>
             </div>
             <div className="homepage_body_div border d-flex flex-row">
                 <div className="homepage_left border">
