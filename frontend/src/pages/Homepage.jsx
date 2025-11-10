46ceeeee8786e076a920e2f1d8d8ff1f179d6990
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-10 14:06:11 +0800

    homepage rough layout

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,19 @@
 function Homepage() {
 
     return (
-        <>
-            <span>Homepage...</span>
-        </>
+        <div className="">
+            <div className="homepage_head w-100 border">
+                Nav Bar
+            </div>
+            <div className="homepage_body_div border d-flex flex-row">
+                <div className="homepage_left border">
+                    left side nav
+                </div>
+                <div className="homepage_main_body border">
+                    body
+                </div>
+            </div>
+        </div>
     )
 }
 
