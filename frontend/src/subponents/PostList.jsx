13153437c665cc11c43b53f594eb332f9f4bc6cb
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-11 14:58:30 +0800

    each post component

--- frontend/src/subponents/PostList.jsx
+++ frontend/src/subponents/PostList.jsx
@@ -1,14 +1,23 @@
-function PostList() {
+function PostList({ posts }) {
 
     return (
-        <div className="each_content_div border  mt-3 mb-3">
-            <div className="each_content_top border w-100">
-            </div>
-            <div className="each_contents_content border w-100">
-                post component
-            </div>
-            <div className="each_content_bottom border w-100"></div>
-        </div>
+        <>
+            {posts.map((post) => (
+                <div key={index} className="each_content_div border  mt-3 mb-3">
+                    <div className="each_content_top border w-100 d-flex justify-content-betweeen">
+                        <div>user id: {post.author}</div>
+                        <div>
+                            { new Date(post.date).toLocaleDateString() } { new Date(post.date).toLocaleTimeString() }
+                        </div>
+                    </div>
+                    <div className="each_contents_content border w-100">
+                        <div className="border w-100">{post.title}</div>
+                        <div className="border w-100">{post.content}</div>
+                    </div>
+                    <div className="each_content_bottom border w-100"></div>
+                </div>
+            ))};
+        </>
     )
 }
 
