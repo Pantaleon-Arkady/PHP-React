280f6e61b96883cec2bc9dabb10b8713a20df9cf
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-11 15:25:53 +0800

    Post list component fixed

--- frontend/src/subponents/PostList.jsx
+++ frontend/src/subponents/PostList.jsx
@@ -1,24 +1,31 @@
 function PostList({ posts }) {
-
+    if (!Array.isArray(posts) || posts.length === 0) {
+      return <div>No posts available.</div>;
+    }
+  
     return (
-        <>
-            {posts.map((post) => (
-                <div key={index} className="each_content_div border  mt-3 mb-3">
-                    <div className="each_content_top border w-100 d-flex justify-content-betweeen">
-                        <div>user id: {post.author}</div>
-                        <div>
-                            { new Date(post.date).toLocaleDateString() } { new Date(post.date).toLocaleTimeString() }
-                        </div>
-                    </div>
-                    <div className="each_contents_content border w-100">
-                        <div className="border w-100">{post.title}</div>
-                        <div className="border w-100">{post.content}</div>
-                    </div>
-                    <div className="each_content_bottom border w-100"></div>
-                </div>
-            ))};
-        </>
-    )
-}
-
-export default PostList;
+      <>
+        {posts.map((post, index) => (
+          <div key={index} className="each_content_div border mt-3 mb-3">
+            <div className="each_content_top border w-100 d-flex justify-content-between p-2">
+              <div>User ID: {post.author}</div>
+              <div>
+                {new Date(post.created_at).toLocaleDateString()}{" "}
+                {new Date(post.created_at).toLocaleTimeString()}
+              </div>
+            </div>
+  
+            <div className="each_contents_content border w-100 p-2">
+              <h5>{post.title}</h5>
+              <p>{post.content}</p>
+            </div>
+  
+            <div className="each_content_bottom border w-100"></div>
+          </div>
+        ))}
+      </>
+    );
+  }
+  
+  export default PostList;
+  
