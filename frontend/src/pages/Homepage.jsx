30e5c1d8131996bfb5e1999a02c891219aab5249
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-21 09:31:22 +0800

    user logged in logic to display homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -10,6 +10,7 @@ function Homepage() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [createPost, setCreatePost] = useState(false);
+    const [user, setUser] = useState(false);
     const location = useLocation();
 
     async function fetchPosts() {
@@ -32,6 +33,9 @@ function Homepage() {
     }
 
     useEffect(() => {
+        setUser(
+            sessionStorage.getItem("user")
+        )
         fetchPosts();
     }, []);
 
@@ -43,57 +47,63 @@ function Homepage() {
     }, [location.state]);
 
     return (
-        <div className="">
-            <div className="homepage_head w-100 border d-flex flex-row">
-                <div className="homepage_head_one border d-flex justify-content-center align-items-center">
-                    <h2>App Name</h2>
-                </div>
-                <div className="homepage_head_two border d-flex justify-content-center align-items-center">
-                    <Button 
-                        className="homepage_head_center" 
-                        variant="secondary"
-                        onClick={() => setCreatePost(true)}
-                    >
-                        Create a Post?
-                    </Button>
-                </div>
-                <div className="homepage_head_three border d-flex justify-content-around align-items-center">
-                    <div className="homepage_head_features border"></div>
-                    <div className="homepage_head_features border"></div>
-                    <div className="homepage_head_features border"></div>
-                </div>
-            </div>
-            <div className="homepage_body_div border d-flex flex-row">
-                <div className="homepage_left border d-flex flex-column">
-                    <div className="homepage_left_sections border w-100 d-flex flex-column justify-content-around align-items-center">
-                        <div className="homepage_left_features border"></div>
-                        <div className="homepage_left_features border"></div>
-                        <div className="homepage_left_features border"></div>
-                        <div className="homepage_left_features border"></div>
+        <>
+            {user ?
+                <div className="">
+                    <div className="homepage_head w-100 border d-flex flex-row">
+                        <div className="homepage_head_one border d-flex justify-content-center align-items-center">
+                            <h2>App Name</h2>
+                        </div>
+                        <div className="homepage_head_two border d-flex justify-content-center align-items-center">
+                            <Button
+                                className="homepage_head_center"
+                                variant="secondary"
+                                onClick={() => setCreatePost(true)}
+                            >
+                                Create a Post?
+                            </Button>
+                        </div>
+                        <div className="homepage_head_three border d-flex justify-content-around align-items-center">
+                            <div className="homepage_head_features border"></div>
+                            <div className="homepage_head_features border"></div>
+                            <div className="homepage_head_features border"></div>
+                        </div>
                     </div>
-                    <div className="homepage_left_sections border w-100 d-flex flex-column justify-content-around align-items-center">
+                    <div className="homepage_body_div border d-flex flex-row">
+                        <div className="homepage_left border d-flex flex-column">
+                            <div className="homepage_left_sections border w-100 d-flex flex-column justify-content-around align-items-center">
+                                <div className="homepage_left_features border"></div>
+                                <div className="homepage_left_features border"></div>
+                                <div className="homepage_left_features border"></div>
+                                <div className="homepage_left_features border"></div>
+                            </div>
+                            <div className="homepage_left_sections border w-100 d-flex flex-column justify-content-around align-items-center">
+                            </div>
+                            <div className="homepage_left_sections border w-100 d-flex flex-column justify-content-around align-items-center">
+                            </div>
+                            <div className="homepage_left_sections border w-100"></div>
+                        </div>
+                        <div className="homepage_main_body border d-flex flex-row">
+                            <div className="homepage_contents_div border d-flex flex-column align-items-center">
+                                {loading && <span>Loading Posts...</span>}
+                                {!loading && !error && (
+                                    <PostList
+                                        posts={posts}
+                                    />
+                                )}
+                            </div>
+                            <div className="homepage_main_side border"></div>
+                        </div>
                     </div>
-                    <div className="homepage_left_sections border w-100 d-flex flex-column justify-content-around align-items-center">
-                    </div>
-                    <div className="homepage_left_sections border w-100"></div>
+                    <CreatePost
+                        show={createPost}
+                        onClose={() => setCreatePost(false)}
+                    />
                 </div>
-                <div className="homepage_main_body border d-flex flex-row">
-                    <div className="homepage_contents_div border d-flex flex-column align-items-center">
-                        {loading && <span>Loading Posts...</span>}
-                        {!loading && !error && (
-                            <PostList 
-                                posts={posts}
-                            />
-                        )}
-                    </div>
-                    <div className="homepage_main_side border"></div>
-                </div>
-            </div>
-            <CreatePost 
-                show={createPost}
-                onClose={() => setCreatePost(false)}
-            />
-        </div>
+                :
+                <div>Not logged in</div>
+            }
+        </>
     )
 }
 
