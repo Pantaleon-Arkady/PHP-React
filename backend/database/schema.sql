2177dfea2e57511bd30aff09791b7af13a6b0ed2
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-11-21 13:14:56 +0800

    like table and like dummy data

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -12,6 +12,14 @@ CREATE TABLE IF NOT EXISTS app_user_posts (
     author INT NOT NULL REFERENCES app_user(id),
     title TEXT,
     content TEXT NOT NULL,
+    like_count INT DEFAULT 0,
+    dislike_count INT DEFAULT 0,
     created_at TIMESTAMP,
     modified_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS app_user_likes (
+    id SERIAL PRIMARY KEY,
+    post INT NOT NULL REFERENCES app_user_posts(id),
+    author INT NOT NULL REFERENCES app_user(id)
 );
--- backend/database/seed.sql
+++ backend/database/seed.sql
@@ -1,32 +1,43 @@
-TRUNCATE app_user, app_user_posts RESTART IDENTITY;
+TRUNCATE app_user, app_user_posts, app_user_likes RESTART IDENTITY;
 
 INSERT INTO app_user (email, role, username, password)
 VALUES 
-  ('bobross@email.com', 'user', 'bob_ross', md5('practice')),
-  ('admin@shop.com', 'admin', 'admin', md5('admin123')),
-  ('first@email.com', 'user', 'first_user', md5('password')),
-  ('demo@demo.com', 'user', 'demo', md5('demo123')),
-  ('test@test.com', 'user', 'test', md5('test123'));
+    ('bobross@email.com', 'user', 'bob_ross', md5('practice')),
+    ('admin@shop.com', 'admin', 'admin', md5('admin123')),
+    ('first@email.com', 'user', 'first_user', md5('password')),
+    ('demo@demo.com', 'user', 'demo', md5('demo123')),
+    ('test@test.com', 'user', 'test', md5('test123'));
+
+INSERT INTO app_user_posts (author, title, content, like_count, created_at)
+VALUES
+    (1,
+    'Talent',
+    '"Talent is a pursued interest. Anything that you''re willing to practice, you can do."',
+    4,
+    '2025-04-28 23:06:00');
 
 INSERT INTO app_user_posts (author, title, content, created_at)
 VALUES
-  (2,
-  'Title',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
-  '2025-06-04 10:45:00'),
-  (2,
-  '2nd Post',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
-  '2025-07-04 10:45:00'),
-  (3,
-  'Title',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
-  '2025-06-05 10:45:00'),
-  (4,
-  'Trial Title',
-  'Trial Post by Demo User',
-  '2025-07-05 10:45:00'),
-  (1,
-  'Talent',
-  '"Talent is a pursued interest. Anything that you''re willing to practice, you can do."',
-  '2025-04-28 23:06:00');
+    (2,
+    'Title',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
+    '2025-06-04 10:45:00'),
+    (2,
+    '2nd Post',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
+    '2025-07-04 10:45:00'),
+    (3,
+    'Title',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non lorem purus. Suspendisse bibendum mauris nunc. Praesent euismod pharetra eleifend.',
+    '2025-06-05 10:45:00'),
+    (4,
+    'Trial Title',
+    'Trial Post by Demo User',
+    '2025-07-05 10:45:00');
+
+INSERT INTO app_user_likes (post, author)
+VALUES
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5);
